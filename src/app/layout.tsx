6a1f133b9380e00ae75d16428fb86dcef4d29b27
Julian <jlopezlopez2007@gmail.com>
2025-09-20 00:23:33 +0200

    Arreglado el error de la pagina principal moviendo el archivo page.tsx a app/src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Inter_Tight } from "next/font/google";
+import { Inter, Inter_Tight } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const interSans = Inter({
   variable: "--font-inter",
